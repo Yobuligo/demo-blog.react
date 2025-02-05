d203c8c6d7d986a4ea06980481f0df44569c19d3
Yobuligo <hoffmannpeter82@gmail.com>
2025-02-05 09:31:51 +0100

    Introduce Todo

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 type Post = {
   id: string;
@@ -9,10 +9,8 @@ type Props = {
   post: Post;
 };
 
-const PostLink: React.FC<Props> = ({ post }) => (
+export const PostLink: React.FC<Props> = ({ post }) => (
   <Link href={`/post/${post.id}`} className="text-blue-500">
     {post.title}
   </Link>
 );
-
-export default PostLink;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import PostLink from "../components/PostLink";
+import { PostLink } from "@/components/PostLink";
 
 type Post = {
   id: string;
