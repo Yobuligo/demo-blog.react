904f214f3072c22e2c0a086fa5fc09000739eee3
Yobuligo <hoffmannpeter82@gmail.com>
2025-02-05 09:59:40 +0100

    Add add amount button

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
 const TestPage: React.FC = () => {
-  const [amount] = useState(0);
+  const [amount, setAmount] = useState(0);
 
-  return <div>{amount}</div>;
+  const onAdd = () =>
+    setAmount((previous) => {
+      return previous + 1;
+    });
+
+  return (
+    <div>
+      {amount}
+      <button onClick={onAdd}>Add</button>
+    </div>
+  );
 };
 
 export default TestPage;
